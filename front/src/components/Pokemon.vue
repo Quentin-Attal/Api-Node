60a3be4df8ab332d261f87787f39f53b7c4258bc
Quentin <56029600+Quentin-Attal@users.noreply.github.com>
2023-01-02 18:07:18 +0100

    responsive

--- front/src/components/Pokemon.vue
+++ front/src/components/Pokemon.vue
@@ -2,8 +2,10 @@
 
 import router from '@/router';
 import { authStore } from '@/store/auth';
+import { onMounted, ref } from 'vue';
 
 const auth = authStore();
+let classCol = ref("col-3")
 
 const props = defineProps<{
     pokemonData: {
@@ -20,12 +22,20 @@ const redirection = () => {
 
 const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.pokemonData.id}.png`
 
+onMounted(() => {
+    changeClass()
+    window.addEventListener('resize', changeClass, { passive: true })
+})
+
+const changeClass = () => {
+    classCol.value = window.innerWidth < 800 ? window.innerWidth < 500 ? 'col-12' : 'col-5' : 'col-3'
+}
 </script>
 
 <template>
-    <button @click="redirection" :style="{cursor:auth.status.loggedIn ? 'pointer': 'default' }"
-        class="col-3 d-flex align-items-center flex-column text-center bg-transparent border-0">
-        <img :src="imageUrl" height="96" width="96" class="w-50" style="aspect-ratio: 1; height: auto;" />
+    <button @click="redirection" :style="{cursor:auth.status.loggedIn ? 'pointer': 'default' }" :class="classCol"
+        class="d-flex align-items-center flex-column text-center bg-transparent border-0">
+        <img :src="imageUrl" height="96" width="96" style="aspect-ratio: 1; height: auto;" />
         <p class="fs-3 text-white">{{ props.pokemonData.name }}</p>
     </button>
 </template>
